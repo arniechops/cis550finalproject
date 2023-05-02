7bdcee3f9de199cee42977d4afaff5c4b4999b6e
Ashwin Balaji <ashwinbalaji@ist-08987.apn.wlan.private.upenn.edu>
2023-05-02 15:25:14 -0400

    route changes

--- backend/app.js
+++ backend/app.js
@@ -17,3 +17,7 @@ app.get('/findairportswithnearbyhotels', routes.findAirportsWithNearbyHotels);
 app.get('/findunited', routes.findUnited);
 app.get('/findflightswithtwostops', routes.findflightswithstops);
 app.get('/findnearbyhotels', routes.findNearbyHotels);
+app.get('/findbusiestairports', routes.findBusiestAirports);
+app.get('/findbusiestairlines', routes.findBusiestAirlines);
+app.get('/flightswiththreestops', routes.flightsWithThreeStops);
+app.get('/findhotelsbysearch', routes.findHotelBySearch);
