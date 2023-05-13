f731d75671de3b388331cf09905e75b4b24392c8
Arnav Chopra <arnav.chopra@gmail.com>
2023-05-13 13:54:27 -0700

    commit

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,6 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
-  if (!url.startsWith('/app/')) // since we're on local windows
-    url = url.substring(1);
-  res.sendFile(url);
-});
-
 app.get('/getflights', routes.getFlights)
 app.get('/gettrip', routes.getTrip)
 app.get('/findhotelswithincomingflights', routes.findHotelsWithIncomingFlights);
