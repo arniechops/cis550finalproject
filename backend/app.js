dc3ba6c5e14994cb0158fc996a74dbed44afcd02
Arnav Chopra <arnav.chopra@gmail.com>
2023-05-13 23:47:07 -0700

    commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Serve the React JSX page at the / endpoint
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../frontend/index.jsx'));
+  res.sendFile(path.join(__dirname, '../frontend/src/index.jsx'));
 });
 
 const port = process.env.PORT || 8000
